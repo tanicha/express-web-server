a6bd97a33be4c5b91f5ce949f895d39463449432
tanicha <tanijcha@gmail.com>
2023-03-03 09:55:36 -0800

    express exercise: created server, port, index file

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express')
 
+const app = express()
+
+const port = 3000
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
